037e280cbd0968e11dea4a547e26c382f58cb993
IUB_LOKI <ayoubouharda33@gmail.com>
2026-02-04 12:28:07 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::middleware(['auth','permission:view-products']) -> group(function ()
+{
+    Route::get('/products',[ProductController::class,'index']);
+});
+Route::middleware(['auth','role:admin']) ->group(function ()
+{
+    //Route::get('/admin',[AdminController:class,'index']);
+});
